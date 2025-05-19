3ab52c5b1869e53dae0edf801752abec57a5bb27
Andre Kaltenbach <andre.kaltenbach@posteo.de>
2025-05-19 15:07:17 +0200

    feat: add signupPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import HomePage from "./pages/HomePage";
-import ProjectListPage from "./pages/ProjectListPage";
-import ProjectDetailsPage from "./pages/ProjectDetailsPage";
-import EditProjectPage from "./pages/EditProjectPage";
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import ProjectListPage from './pages/ProjectListPage';
+import ProjectDetailsPage from './pages/ProjectDetailsPage';
+import EditProjectPage from './pages/EditProjectPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
 
-      <Routes>      
+      <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/projects" element={<ProjectListPage />} />
         <Route exact path="/projects/:projectId" element={<ProjectDetailsPage />} />
-        <Route exact path="/projects/edit/:projectId" element={<EditProjectPage />} />           
+        <Route exact path="/projects/edit/:projectId" element={<EditProjectPage />} />
+        <Route exact path="/signup" element={<SignupPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,57 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+
+const API_URL = 'http://localhost:5005';
+
+function SignupPage() {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState(undefined);
+
+  const navigate = useNavigate();
+
+  const handleEmail = (e) => setEmail(e.target.value);
+  const handlePassword = (e) => setPassword(e.target.value);
+  const handleName = (e) => setName(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const requestBody = { email, password, name };
+
+    axios
+      .post(`${API_URL}/auth/signup`, requestBody)
+      .then((response) => {
+        console.log('user created');
+        navigate('/login');
+      })
+      .catch((error) => {
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      });
+  };
+
+  return (
+    <div className="SignupPage">
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email:</label>
+        <input type="email" name="email" value={email} onChange={handleEmail} />
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" value={password} onChange={handlePassword} />
+        <label htmlFor="name">Name:</label>
+        <input type="name" name="text" value={name} onChange={handleName} />
+        <button type="submit">Sign Up</button>
+      </form>
+
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+      <p>Already have account?</p>
+      <Link to={'/login'}> Login</Link>
+    </div>
+  );
+}
+
+export default SignupPage;
