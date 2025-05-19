1e1790258636b7a48cb7a09734c8affb3617a49d
Andre Kaltenbach <andre.kaltenbach@posteo.de>
2025-05-19 14:13:02 +0200

    feat: use context in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,39 @@
-import { Link } from "react-router-dom";
+// src/components/Navbar.jsx
+
+import { Link } from 'react-router-dom';
+import { useContext } from 'react'; // <== IMPORT
+import { AuthContext } from '../context/auth.context'; // <== IMPORT
 
 function Navbar() {
+  const { isLoggedIn, user } = useContext(AuthContext); // <== ADD
+
   return (
     <nav>
       <Link to="/">
         <button>Home</button>
       </Link>
 
-      <Link to="/projects">
-        <button>Projects</button>
-      </Link>
+      {isLoggedIn && (
+        <>
+          <Link to="/projects">
+            <button>Projects</button>
+          </Link>
+          <button>Logout</button>
+        </>
+      )}
+
+      {!isLoggedIn && (
+        <>
+          <Link to="/signup">
+            {' '}
+            <button>Sign Up</button>{' '}
+          </Link>
+          <Link to="/login">
+            {' '}
+            <button>Login</button>{' '}
+          </Link>
+        </>
+      )}
     </nav>
   );
 }
--- src/context/auth.context.jsx
+++ src/context/auth.context.jsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect, createContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
 const API_URL = 'http://localhost:5005';
 
-const AuthContext = createContext();
+const AuthContext = React.createContext();
 
 function AuthProviderWrapper(props) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -11,7 +10,7 @@ function AuthProviderWrapper(props) {
   const [user, setUser] = useState(null);
 
   return (
-    <AuthContext.Provider vlaue={{ isLoggedIn, isLoading, user }}>
+    <AuthContext.Provider value={{ isLoggedIn, isLoading, user }}>
       {props.children}
     </AuthContext.Provider>
   );
