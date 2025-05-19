9e527fb5deb8bac1def50b5ec6f6162b5c91be60
Andre Kaltenbach <andre.kaltenbach@posteo.de>
2025-05-19 17:00:30 +0200

    feat: create loginPage

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ProjectListPage from './pages/ProjectListPage';
 import ProjectDetailsPage from './pages/ProjectDetailsPage';
 import EditProjectPage from './pages/EditProjectPage';
 import SignupPage from './pages/SignupPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
@@ -13,11 +14,12 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route exact path="/projects" element={<ProjectListPage />} />
-        <Route exact path="/projects/:projectId" element={<ProjectDetailsPage />} />
-        <Route exact path="/projects/edit/:projectId" element={<EditProjectPage />} />
-        <Route exact path="/signup" element={<SignupPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/projects" element={<ProjectListPage />} />
+        <Route path="/projects/:projectId" element={<ProjectDetailsPage />} />
+        <Route path="/projects/edit/:projectId" element={<EditProjectPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </div>
   );
